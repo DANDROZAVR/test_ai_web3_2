615a8f0d479acc583c068acc339c4bad48c0c571
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-04-14 13:06:38 +0200

    Incorporated code snippets from input files

--- src/caller.py
+++ src/caller.py
@@ -1,1 +1,9 @@
- 
+ class InvalidTransaction(Web3Exception):
+    """
+    Raised when a transaction includes an invalid combination of arguments.
+    """
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
