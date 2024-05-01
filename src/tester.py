2c5cccacefe28b890163c606ee090689467cdac9
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-05-01 13:06:38 +0200

    Integrated reusable code blocks

--- src/tester.py
+++ src/tester.py
@@ -1,2 +1,8 @@
 # Auto-generated file
 System.out.println('User logged in: user90');
+class ProviderConnectionError(Web3Exception):
+    """
+    Raised when unable to connect to a provider
+    """
+
+
