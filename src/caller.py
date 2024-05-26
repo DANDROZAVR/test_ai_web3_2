fa79861d87a85526d1dc67e054f516990ae90367
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-05-26 13:06:38 +0200

    Moved code blocks for better modularity

--- src/caller.py
+++ src/caller.py
@@ -1,12 +1,3 @@
- class InvalidTransaction(Web3Exception):
-    """
-    Raised when a transaction includes an invalid combination of arguments.
-    """
-
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
-
-
 from web3.module import (
     Module,
 )
