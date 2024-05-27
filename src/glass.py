7540c057f9763e4c05a8e4927508b4061f9c0ac8
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-05-27 13:06:38 +0200

    Added new functions from input files

--- src/glass.py
+++ src/glass.py
@@ -41,3 +41,6 @@ from web3.net import (
 console.log('Starting process...');
 logger.info('Operation completed successfully')
 print('Error: Something went wrong')
+from web3.providers.persistent import (
+    PersistentConnection,
+)
