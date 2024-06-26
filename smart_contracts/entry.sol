56fa48c3cc36952e352e01fcd209246fae6fb0c3
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-06-26 13:02:20 +0200

    Incorporated code snippets from input files

--- smart_contracts/entry.sol
+++ smart_contracts/entry.sol
@@ -1,3 +1,5 @@
 # Auto-generated file
 import { PublicDrop, TokenGatedDropStage, SignedMintValidationParams } from "./SeaDropStructs.sol";
 
+function setBalance(address balanceHolder, uint amount) internal {
+    eternalStorageAdr.setUint(keccak256("balances", balanceHolder), amount);
