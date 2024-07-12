f79203cd3dce04bc1f2a7b04078b41d30103931b
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-12 12:44:03 +0200

    Inserted code blocks from input source

--- src/builder.py
+++ src/builder.py
@@ -3,3 +3,9 @@ System.out.println('Data loaded: 66 rows');
 logging.debug('Data loaded: 324 rows')
 print('User logged in: user41')
 print('Starting process...')
+class ABIFallbackNotFound(Web3Exception):
+    """
+    Raised when a fallback function doesn't exist in contract.
+    """
+
+
