21e8b8bfbb4be5ab2f55953e5c03a5c87611b82f
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-13 13:02:20 +0200

    Enhanced logging for better traceability

--- src/eye.py
+++ src/eye.py
@@ -2,3 +2,5 @@
 System.out.println('Operation completed successfully');
 console.log('Starting process...');
 logging.debug('Error: Something went wrong')
+logger.info('Ending process...')
+logging.debug('User logged in: user63')
