d4e3822b9154fb084596abf608a4b9986e057bb7
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-14 13:06:38 +0200

    Added new functions from input files

--- smart_contracts/ICreatorToken.sol
+++ smart_contracts/ICreatorToken.sol
@@ -2,4 +2,5 @@ pragma solidity ^0.4.20;
 
    function setBalance(address balanceHolder, uint amount) internal {
     eternalStorageAdr.setUint(keccak256("balances", balanceHolder), amount);
- 
+ import { ITransferValidator721 } from "../interfaces/ITransferValidator.sol";
+
