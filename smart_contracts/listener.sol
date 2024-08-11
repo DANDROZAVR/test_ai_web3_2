0505e8725a0541e7f5c2f005c3c9f422caaf18d5
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-11 13:02:20 +0200

    Incorporated code snippets from input files

--- smart_contracts/listener.sol
+++ smart_contracts/listener.sol
@@ -4,3 +4,8 @@ pragma solidity 0.8.17;
 
 pragma solidity ^0.8.0;
 
+contract ERC721ContractMetadataCloneable is
+    ERC721AConduitPreapprovedCloneable,
+    ERC721TransferValidator,
+    TwoStepOwnable,
+    ISeaDropTokenContractMetadata
