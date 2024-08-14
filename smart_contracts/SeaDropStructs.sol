c7bab21b6ec14c8f53b16ec670c3a31ace5dae00
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-14 13:06:38 +0200

    Integrated reusable code blocks

--- smart_contracts/SeaDropStructs.sol
+++ smart_contracts/SeaDropStructs.sol
@@ -6,3 +6,8 @@ import { ERC721ACloneable } from "./ERC721ACloneable.sol";
 
 pragma solidity 0.8.17;
 
+import {
+  AllowListData,
+  PublicDrop,
+  SignedMintValidationParams,
+  TokenGatedDropStage
