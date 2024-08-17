bb67f2d7cdeb48ee91db44147c3152a4d422e59c
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-17 13:06:38 +0200

    Added new functions from input files

--- smart_contracts/main.sol
+++ smart_contracts/main.sol
@@ -1,3 +1,4 @@
 import {
     INonFungibleSeaDropToken
-     
+     function getBalance(address balanceHolder) public view returns(uint) {
+    return eternalStorageAdr.getUint(keccak256("balances", balanceHolder));
