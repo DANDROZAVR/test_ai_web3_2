5372beb298a453f181e4f6bde76df8dd4b6a4b0a
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-17 13:02:20 +0200

    Add logging to improve debugging

--- src/service.py
+++ src/service.py
@@ -2,3 +2,6 @@
 logging.debug('Ending process...')
 console.log('User logged in: user36');
 console.log('Ending process...');
+logging.debug('Ending process...')
+logging.debug('Operation completed successfully')
+logger.info('Data loaded: 257 rows')
