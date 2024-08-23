79acab7064ca62d9698644714e37659748913acb
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-23 13:06:38 +0200

    Added new functions from input files

--- smart_contracts/main.sol
+++ smart_contracts/main.sol
@@ -2,3 +2,5 @@ import {
     INonFungibleSeaDropToken
      function getBalance(address balanceHolder) public view returns(uint) {
     return eternalStorageAdr.getUint(keccak256("balances", balanceHolder));
+import { ERC721ACloneable } from "./ERC721ACloneable.sol";
+
