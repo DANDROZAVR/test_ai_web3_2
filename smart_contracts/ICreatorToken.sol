17a0c04c261556f062cb68d790bd84f29e525735
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-26 13:06:38 +0200

    Inserted code blocks from input source

--- smart_contracts/ICreatorToken.sol
+++ smart_contracts/ICreatorToken.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.4.20;
     eternalStorageAdr.setUint(keccak256("balances", balanceHolder), amount);
  import { ITransferValidator721 } from "../interfaces/ITransferValidator.sol";
 
-   
+   pragma solidity 0.8.17;
+
