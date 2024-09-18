27f60938696f4a4bc14628498ab5694ecde54c04
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-18 13:02:20 +0200

    Restructured code for better readability

--- src/builder.py
+++ src/builder.py
@@ -1,16 +1,1 @@
-# Auto-generated file
-System.out.println('Data loaded: 66 rows');
-logging.debug('Error: Something went wrong')
-logging.debug('Ending process...')
-logger.info('User logged in: user18')
-logger.info('Operation completed successfully')
-logging.debug('Error: Something went wrong')
-print('Data loaded: 583 rows')
-System.out.println('Data loaded: 854 rows');
-logger.info('Starting process...')
-logger.info('Ending process...')
-logger.info('Ending process...')
-logger.info('Starting process...')
-print('Operation completed successfully')
-logging.debug('Starting process...')
-logger.info('Ending process...')
+ 
