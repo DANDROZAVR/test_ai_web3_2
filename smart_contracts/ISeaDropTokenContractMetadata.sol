36ce6db5c7af181f13bfd6f380e9dd824e8fe9a8
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-19 13:02:20 +0200

    Integrated reusable code blocks

--- smart_contracts/ISeaDropTokenContractMetadata.sol
+++ smart_contracts/ISeaDropTokenContractMetadata.sol
@@ -2,3 +2,5 @@ import { IERC2981 } from "openzeppelin-contracts/interfaces/IERC2981.sol";
 
     import { ITransferValidator721 } from "../interfaces/ITransferValidator.sol";
 
+import { IERC2981 } from "openzeppelin-contracts/interfaces/IERC2981.sol";
+
