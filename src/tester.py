d792fe6ca1f9d0136f73898c5d44961fc91a71e2
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-20 13:02:20 +0200

    Added new log statements

--- src/tester.py
+++ src/tester.py
@@ -1,3 +1,5 @@
  logger.info('Data loaded: 710 rows')
 console.log('Configuration updated');
 logger.info('Operation completed successfully')
+print('Configuration updated')
+logger.info('Data loaded: 924 rows')
