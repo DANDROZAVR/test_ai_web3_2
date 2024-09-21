f71dd93e70525e166fb1daa03405e24e5b6151f8
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-21 13:02:20 +0200

    Add logging to improve debugging

--- src/service.py
+++ src/service.py
@@ -7,3 +7,6 @@ logging.debug('Operation completed successfully')
 logger.info('Data loaded: 257 rows')
 logger.info('Configuration updated')
 console.log('Starting process...');
+logger.info('Configuration updated')
+System.out.println('Configuration updated');
+logger.info('Configuration updated')
