061ff3065c89561ea16d82f9fa095b79d7b7ea2f
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-22 13:02:20 +0200

    Restructured code for better readability

--- src/service.py
+++ src/service.py
@@ -1,12 +1,1 @@
-# Auto-generated file
-logging.debug('Ending process...')
-console.log('User logged in: user36');
-console.log('Ending process...');
-logging.debug('Ending process...')
-logging.debug('Operation completed successfully')
-logger.info('Data loaded: 257 rows')
-logger.info('Configuration updated')
-console.log('Starting process...');
-logger.info('Configuration updated')
-System.out.println('Configuration updated');
-logger.info('Configuration updated')
+ 
