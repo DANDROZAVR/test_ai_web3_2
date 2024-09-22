d9adf679b8a546c4d895abad8f5ce33969f5fd96
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-22 13:02:20 +0200

    Reorganized functions for clarity

--- src/eye.py
+++ src/eye.py
@@ -1,10 +1,1 @@
-# Auto-generated file
-System.out.println('Operation completed successfully');
-console.log('Starting process...');
-logging.debug('Error: Something went wrong')
-logger.info('Ending process...')
-logging.debug('User logged in: user63')
-print('Configuration updated')
-console.log('Configuration updated');
-logging.debug('Ending process...')
-logger.info('Starting process...')
+ 
