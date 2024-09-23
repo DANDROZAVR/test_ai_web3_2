2add6c5e1b008d9af998baa51841e38b21cef792
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-23 13:06:38 +0200

    Reorganized functions for clarity

--- src/app.py
+++ src/app.py
@@ -5,5 +5,4 @@ from web3.module import (
 
   console.log('User logged in: user17');
   System.out.println('Operation completed successfully');
-logger.info('User logged in: user91')
-console.log('Data loaded: 556 rows');
+ 
