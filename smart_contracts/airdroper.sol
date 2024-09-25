998ab8fb2fc9df9fc9e06c9434267c24ae5c675e
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-25 13:02:20 +0200

    Added new functions from input files

--- smart_contracts/airdroper.sol
+++ smart_contracts/airdroper.sol
@@ -2,3 +2,5 @@
 @notice A two-step extension of Ownable, where the new owner must claim ownership of the contract after owner initiates transfer
 pragma solidity ^0.8.4;
 
+import { IERC721A } from "ERC721A/IERC721A.sol";
+
