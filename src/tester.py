9d9c98319127ded401af9c09cecdd5c7b0565c57
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-09-27 13:02:20 +0200

    Moved code blocks for better modularity

--- src/tester.py
+++ src/tester.py
@@ -1,5 +1,1 @@
- logger.info('Data loaded: 710 rows')
-console.log('Configuration updated');
-logger.info('Operation completed successfully')
-print('Configuration updated')
-logger.info('Data loaded: 924 rows')
+ 
