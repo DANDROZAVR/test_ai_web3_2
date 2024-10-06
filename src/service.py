71c6e4b156ccb696f4453a15e11200e2572c69cd
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-10-06 13:06:38 +0200

    Inserted code blocks from input source

--- src/service.py
+++ src/service.py
@@ -103,3 +103,10 @@ from collections import (
 from collections.abc import (
     Hashable,
 )
+class TransactionIndexingInProgress(Web3RPCError):
+    """
+    Raised when a transaction receipt is not yet available due to transaction indexing
+    still being in progress.
+    """
+
+
