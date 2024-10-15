11f172f7f1eb592afed5eeb4bd64a655637e2800
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-10-15 13:02:20 +0200

    Incorporated code snippets from input files

--- smart_contracts/entry.sol
+++ smart_contracts/entry.sol
@@ -5,3 +5,10 @@ function setBalance(address balanceHolder, uint amount) internal {
     eternalStorageAdr.setUint(keccak256("balances", balanceHolder), amount);
 pragma solidity 0.8.17;
 
+import {
+    AllowListData,
+    MintParams,
+    PublicDrop,
+    TokenGatedDropStage,
+    TokenGatedMintParams,
+    SignedMintValidationParams
