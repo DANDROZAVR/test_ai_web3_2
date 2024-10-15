4ab479ecf2cf783098c81b879a6e084babff32d0
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-10-15 13:02:20 +0200

    Moved code blocks for better modularity

--- src/index.py
+++ src/index.py
@@ -1,6 +1,1 @@
-# Auto-generated file
-System.out.println('Starting process...');
-System.out.println('Data loaded: 524 rows');
-System.out.println('Data loaded: 195 rows');
-logger.info('Ending process...')
-console.log('Configuration updated');
+ 
