447238a5eba2ab7d4ec8bdcd9d500cda19beba82
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-02 13:02:20 +0100

    Added new log statements

--- src/node.py
+++ src/node.py
@@ -1,3 +1,5 @@
  console.log('Data loaded: 374 rows');
 print('Operation completed successfully')
 logging.debug('Operation completed successfully')
+logger.info('Operation completed successfully')
+logging.debug('Data loaded: 200 rows')
