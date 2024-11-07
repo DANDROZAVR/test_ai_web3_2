e9d1a291400fcf1d76413819623fef0cb13014e0
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-07 13:06:38 +0100

    Restructured code for better readability

--- src/index.py
+++ src/index.py
@@ -1,13 +1,3 @@
- def apply_result_formatters(
-    result_formatters: Callable[..., Any], result: RPCResponse
-) -> RPCResponse:
-    if result_formatters:
-        formatted_result = pipe(result, result_formatters)
-        return formatted_result
-    else:
-        return result
-
-
 class NameNotFound(Web3Exception):
     """
     Raised when a caller provides an Ethereum Name Service name that
@@ -21,16 +11,10 @@ class MethodUnavailable(Web3RPCError):
     """
 
 
-System.out.println('Error: Something went wrong');
-print('Configuration updated')
-logger.info('User logged in: user9')
 from web3.net import (
     AsyncNet,
     Net,
 )
-logger.info('Ending process...')
-logging.debug('Data loaded: 250 rows')
-logger.info('Ending process...')
 from web3.providers import (
     AsyncBaseProvider,
     BaseProvider,
