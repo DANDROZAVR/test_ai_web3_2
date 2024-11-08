7bb80ed1a59c532e067b886e1a26b367b7c39631
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-08 13:02:20 +0100

    Added new functions from input files

--- smart_contracts/ISeaDropTokenContractMetadata.sol
+++ smart_contracts/ISeaDropTokenContractMetadata.sol
@@ -13,3 +13,5 @@ import {
     SignedMintValidationParams
 import { IERC2981 } from "openzeppelin-contracts/interfaces/IERC2981.sol";
 
+import { IERC2981 } from "openzeppelin-contracts/interfaces/IERC2981.sol";
+
