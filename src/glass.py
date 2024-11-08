e892c508a3f40c49d0038fdd26d7eade5755530e
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-08 13:02:20 +0100

    Added new log statements

--- src/glass.py
+++ src/glass.py
@@ -1,3 +1,5 @@
  System.out.println('Error: Something went wrong');
 logger.info('Data loaded: 739 rows')
 print('Configuration updated')
+print('Starting process...')
+console.log('Starting process...');
