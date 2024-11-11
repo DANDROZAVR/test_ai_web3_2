cd8d63569c78489bcc81331c54b6a87fb00ed82d
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-11 13:02:20 +0100

    Reorganized functions for clarity

--- smart_contracts/ERC721AConduitPreapprovedCloneable.sol
+++ smart_contracts/ERC721AConduitPreapprovedCloneable.sol
@@ -1,6 +1,6 @@
+pragma solidity 0.8.17;
+
 pragma solidity ^0.8.4;
 
 import { PublicDrop, TokenGatedDropStage, SignedMintValidationParams } from "./SeaDropStructs.sol";
-
-pragma solidity 0.8.17;
 
