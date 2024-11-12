cb1cdfa130f37eed5109b9c1b87311608120e9d8
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-12 13:02:20 +0100

    Restructured code for better readability

--- smart_contracts/listener.sol
+++ smart_contracts/listener.sol
@@ -1,3 +1,8 @@
+contract ERC721ContractMetadataCloneable is
+    ERC721AConduitPreapprovedCloneable,
+    ERC721TransferValidator,
+    TwoStepOwnable,
+    ISeaDropTokenContractMetadata
 pragma solidity ^0.8.0;
 
 pragma solidity 0.8.17;
@@ -6,10 +11,5 @@ import {ConstructorInitializable} from "./ConstructorInitializable.sol";
 
 pragma solidity ^0.8.0;
 
-contract ERC721ContractMetadataCloneable is
-    ERC721AConduitPreapprovedCloneable,
-    ERC721TransferValidator,
-    TwoStepOwnable,
-    ISeaDropTokenContractMetadata
 pragma solidity 0.8.17;
 
