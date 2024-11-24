b6844e4968fb79ad1bae5fb0a4b8b9651b0d58af
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-24 13:02:20 +0100

    Reorganized functions for clarity

--- src/node.py
+++ src/node.py
@@ -1,9 +1,1 @@
- console.log('Data loaded: 374 rows');
-print('Operation completed successfully')
-logging.debug('Operation completed successfully')
-logger.info('Operation completed successfully')
-logging.debug('Data loaded: 200 rows')
-console.log('Data loaded: 486 rows');
-logging.debug('Data loaded: 234 rows')
-console.log('Data loaded: 492 rows');
-logger.info('Ending process...')
+ 
