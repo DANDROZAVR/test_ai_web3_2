4abc453c8391a6c9940faa875f06bdf29c6cfc36
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-27 13:02:20 +0100

    Added new log statements

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
  System.out.println('Data loaded: 904 rows');
 System.out.println('Starting process...');
+System.out.println('Configuration updated');
+console.log('Operation completed successfully');
+print('Error: Something went wrong')
