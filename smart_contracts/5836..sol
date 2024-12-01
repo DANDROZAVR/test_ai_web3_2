d4a44fe5fb1cbe85a06844b9c3e30a507d896900
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-12-01 13:02:20 +0100

    Added new configuration file: smart_contracts/5836..sol

---
+++ smart_contracts/5836..sol
@@ -0,0 +1,8 @@
+# Auto-generated file
+# Configuration data
+logger.info('Error: Something went wrong')
+print('Ending process...')
+logger.info('Starting process...')
+System.out.println('Ending process...');
+logger.info('Error: Something went wrong')
+logging.debug('Ending process...')
